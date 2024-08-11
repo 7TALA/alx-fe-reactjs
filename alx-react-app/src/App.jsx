00c5586a9886960d41b35464aa36eb8070d4cb01
7tala <zarougtala@gmail.com>
2024-08-11 21:43:49 +0200

    updated

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './WelcomeMessage'
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
-import UserProfile from './UserProfile'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,11 +14,6 @@ function App() {
   return (
     <>
       <div>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <Footer />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,6 +22,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
