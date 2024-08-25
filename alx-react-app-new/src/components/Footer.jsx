04a094272560bf3c247084c17e890518e2b6b26e
7tala <zarougtala@gmail.com>
2024-08-25 15:38:52 +0200

    task 0

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return(
         <footer>
-            <p>© 2023 City Lovers</p>
+            <p style={{fontSize:"2rem"}}>© 2023 City Lovers</p>
         </footer>
     )
 }
