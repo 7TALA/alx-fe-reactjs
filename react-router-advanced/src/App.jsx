29d25c25da2433de75596d7541097299003357e0
7tala <zarougtala@gmail.com>
2024-09-01 10:13:56 +0200

    update

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,48 +1,36 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import "./App.css";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
-import Navbar from "./components/Navbar";
-import ProfileDetails from "./components/ProfileDetails";
-import ProfileSettings from "./components/ProfileSettings";
-import Blog from "./components/Blog";
-import ProtectedRoute from "./components/ProtectedRoute";
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import './App.css'
+import Home from './components/Home'
+import Profile from './components/Profile'
+import Navbar from './components/Navbar'
+import ProfileDetails from './components/ProfileDetails'
+import ProfileSettings from './components/ProfileSettings'
+import Blog from './components/Blog'
+import BlogPost from './components/BlogPost'
+import { AuthProvider } from './components/auth'
+import Login from './components/Login'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Profile" element={<Profile />}>
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
-        <Route path="/blog/:id" element={<BlogPost />} />
-      </Routes>
-    </>
-  );
-}
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        // Navigation links
-        <ul>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-        // Login page route
-        <Route path="/login" component={Login} />
-        // Protected profile page route
-        <ProtectedRoute path="/profile" component={Profile} />
-      </div>
-    </BrowserRouter>
-  );
-};
 
-export default App;
+  return (
+    <AuthProvider>
+    <Navbar />
+     <Routes>
+        <Route path= "/" element={<Home />} />
+        <Route path= "/profile" element={
+          <ProtectedRoute>
+           <Profile />
+          </ProtectedRoute>}> 
+          <Route path= "details" element={<ProfileDetails />} />
+          <Route path= "settings" element={<ProfileSettings />} />
+        </ Route>
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/blog/:id' element={<BlogPost />}/>
+        <Route path='/login' element={<Login />}/>
+     </Routes>
+    </AuthProvider>
+  )
+}
+
+export default App
--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,39 +1,17 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from 'react'
+import { useAuth } from './auth'
+import { Navigate, useLocation } from 'react-router-dom'
 
-// Fake authentication object
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate() {
-    fakeAuth.isAuthenticated = true;
-  },
-  signout() {
-    fakeAuth.isAuthenticated = false;
-  },
-};
 
-// ProtectedRoute component definition
-const ProtectedRoute = ({ component: Component, ...rest }) => {
-  return (
-    // Route component from react-router-dom
-    <Route
-      {...rest}
-      render={(props) =>
-        // Check if user is authenticated
-        fakeAuth.isAuthenticated ? (
-          // If authenticated, render the protected component
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: '/login',
-              state: { from: props.location },
-            }}
-          />
-        )
-      }
-    />
-  );
-};
+const ProtectedRoute = ({children}) => {
+    const auth = useAuth()
+    const location = useLocation()
 
-export default ProtectedRoute;
+    if(!auth.user){
+        return <Navigate to='/Login' state={{path: location.pathname}} />
+    }
+
+  return children
+}
+
+export default ProtectedRoute
