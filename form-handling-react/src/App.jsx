a38f6eb36a23dc69d07541eb47cbfabfdb4d362c
7tala <zarougtala@gmail.com>
2024-09-01 09:52:06 +0200

    task 0

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import RegistrationForm from './components/RegistrationForm'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <RegistrationForm />
+    </>
+  )
+}
+
+export default App
